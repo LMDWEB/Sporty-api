4c64e893497811fc27ccc6b9ccb3b79312d772e8
Kevin <kev_kevin@hotmail.fr>
2019-05-21 15:43:33 +0200

    remove entities

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Club;
-use App\Entity\Player;
 use App\Entity\Role;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -19,43 +17,6 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $joueurs = json_decode(file_get_contents("https://raw.githubusercontent.com/LMDWEB/Sporty/master/public/json/playerL1.json"));
-
-        $clubs = array();
-        $realClubs = [];
-
-        foreach ($joueurs as $joueur){
-            $clubs[$joueur->{'Current club'}] = $joueur->{'Current club'};
-        }
-
-        foreach($clubs as $club){
-            $team = new Club();
-            $team->setName($club);
-            $manager->persist($team);
-            $realClubs[$team->getName()] = $team;
-        }
-
-        foreach ($joueurs as $joueur) {
-            $foot = (isset($joueur->Foot)) ? $joueur->Foot : "right";
-            $club = $realClubs[$joueur->{'Current club'}];
-            if(!isset($joueur->Height)) {$height = 0;} else {$height = $joueur->Height;}
-            $height = str_replace(',', '.', $height);
-            $height = str_replace('m', '', $height);
-            $height = str_replace(' ', '', $height);
-            $height = floatval($height);
-
-            $player = new Player();
-
-            $player->setFullName($joueur->Name)
-                ->setPosition($joueur->Position)
-                ->setHeight($height)
-                ->setBirthdayDate(time())
-                ->setAge($joueur->Age)
-                ->setFoot($foot)
-                ->setClub($club);
-
-            $manager->persist($player);
-        }
 
         $adminRole = new Role();
         $adminRole->setTitle("ROLE_ADMIN");
