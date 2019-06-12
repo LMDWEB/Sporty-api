434d9735c83c2dab405bbd1a9a670cba9fc6ff87
Kevin <kev_kevin@hotmail.fr>
2019-06-12 11:17:55 +0200

    update comment

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -3,16 +3,11 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
-
-use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CommentRepository")
- * @ApiResource(
- *     normalizationContext={"groups"={"read"}},
- *     denormalizationContext={"groups"={"write"}}
- * )
+ * @ApiResource()
  */
 class Comment
 {
@@ -20,31 +15,26 @@ class Comment
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @Groups({"read", "write"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="text")
-     * @Groups({"read", "write"})
      */
     private $content;
 
     /**
      * @ORM\Column(type="datetime")
-     * @Groups({"read", "write"})
      */
     private $createdAt;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Game", inversedBy="comments")
-     * @Groups({"read", "write"})
      */
     private $game;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="comments")
-     * @Groups({"read", "write"})
      */
     private $creator;
 
